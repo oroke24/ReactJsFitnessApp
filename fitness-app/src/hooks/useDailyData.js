e3020647627b6644d2e1a85fab24a27c86652e60
oroke24 <jamieoroke@yahoo.com>
2025-05-18 01:48:29 -0700

    Adding day edit area to bottom of calendar

---
+++ fitness-app/src/hooks/useDailyData.js
@@ -0,0 +1,48 @@
+// src/hooks/useDayData.js
+import { useState, useEffect } from 'react';
+import { dayDataManager } from '../firebase/dayDataManager';
+
+const useDailyData = (selectedDate, userEmail) => {
+  const [dayData, setDayData] = useState({
+    date: '',
+    recipes: [],
+    exercises: [],
+  });
+
+  useEffect(() => {
+    if (!selectedDate || !userEmail) return;
+
+    const fetchDay = async () => {
+      const dayManager = new dayDataManager(userEmail);
+      const formattedDate = selectedDate.toISOString().split('T')[0];
+      const day = await dayManager.getDayFromDate(formattedDate);
+
+      const exercises = [
+        day.exercise1Id,
+        day.exercise2Id,
+        day.exercise3Id,
+        day.exercise4Id,
+        day.exercise5Id,
+      ].filter(Boolean);
+
+      const recipes = [
+        day.recipe1Id,
+        day.recipe2Id,
+        day.recipe3Id,
+        day.recipe4Id,
+        day.recipe5Id,
+      ].filter(Boolean);
+
+      setDayData({
+        date: formattedDate,
+        exercises,
+        recipes
+      });
+    };
+    fetchDay();
+  }, [selectedDate, userEmail]);
+
+  return dayData;
+};
+
+export default useDailyData;
