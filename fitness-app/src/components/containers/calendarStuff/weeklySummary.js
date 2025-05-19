0c3a7716bbbbdd0fed312d37ea105647119ad512
oroke24 <jamieoroke@yahoo.com>
2025-05-19 15:16:38 -0700

    styling

--- fitness-app/src/components/containers/calendarStuff/weeklySummary.js
+++ fitness-app/src/components/containers/calendarStuff/weeklySummary.js
@@ -4,7 +4,7 @@ import DayComponent from './DayComponent';
 const WeeklySummary = ({ selectedDate, days, onDayClick }) => {
     console.log("days in weeklySummary:", days);
     return (
-        <div className='flex pl-20 w-[90vh] overflow-x-auto space-x-4 p-2 m-5'>
+        <div className='flex pl-40 w-[90vh] overflow-x-auto space-x-4 p-2 m-5'>
             {days.map((day, index) => (
                 <div
                     className={`p-1`}
