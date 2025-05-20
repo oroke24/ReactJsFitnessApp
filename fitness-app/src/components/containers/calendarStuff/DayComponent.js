67505c82f3ebd9f23ba8230296b628ff97730862
oroke24 <jamieoroke@yahoo.com>
2025-05-19 20:48:24 -0700

    styling

--- fitness-app/src/components/containers/calendarStuff/DayComponent.js
+++ fitness-app/src/components/containers/calendarStuff/DayComponent.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './calendar.css';
 
 const DayComponent = ({ date, recipes=[], exercises = []}) => {
     let myDay = new Date(date);//IMPORTANT: For some rease myDay is set to day before
@@ -7,7 +8,7 @@ const DayComponent = ({ date, recipes=[], exercises = []}) => {
     myDay = myDay.toDateString();// so we actually stringify the correct day.
 
     return (
-        <div className='border p-2 pt-1 rounded shadow mb-4 h-[250px] w-[350px] overflow-y-auto'>
+        <div className='border p-2 pt-1 rounded shadow mb-4 h-[250px] w-[350px] overflow-y-auto day-gradient'>
             <h3 className='font-bold text-center mb-3'>{myDay}</h3>
             <div className='flex justify-between'>
             <div className='flex-1 border'>
