c82194b09da8e1fc08b4ecf127c137a57ec7b278
oroke24 <jamieoroke@yahoo.com>
2025-10-16 22:55:20 -0700

    Feat: styling

--- fitness-app/src/components/containers/PrintMyExercises.js
+++ fitness-app/src/components/containers/PrintMyExercises.js
@@ -6,17 +6,18 @@ import './mobileCompatConfig.css';
 import { Link } from 'react-router-dom';
 import { deleteDocument } from '../../firebase/firebaseFirestore';
 import ExerciseSwipable from './ExerciseSwipable';
+import { FaPlus, FaRunning } from 'react-icons/fa';
 
-const PrintMyExercises = ({path}) => {
-    const [isLoading, setLoading]= useState(false);
-    var {docs, loading, error} = useGetDocs(`${path}/exercises`);
+const PrintMyExercises = ({ path }) => {
+    const [isLoading, setLoading] = useState(false);
+    var { docs, loading, error } = useGetDocs(`${path}/exercises`);
     //console.log(path)
 
-    if(loading) return <p>Loading...</p>;
-    if(error) return <p>Error: {error}</p>;
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error: {error}</p>;
 
     const handleDelete = async (name) => {
-        if(window.confirm(`Are you sure you want to delete ${name}?`)){
+        if (window.confirm(`Are you sure you want to delete ${name}?`)) {
             setLoading(true);
             await deleteDocument('exercises', name);
             setLoading(false);
@@ -29,33 +30,27 @@ const PrintMyExercises = ({path}) => {
     );
 
     return (
-    <div>
+        <div>
             {isLoading && (
                 <div className="loading-screen">
                     <div className="loading-spinner"></div>
                 </div>
             )}
-        <h1 className="text-4xl text-center mb-5">Exercises</h1>
-        {/*New Card Button */}
-        <div className='w-full flex justify-center'>
-            <Link to ={`editExercise/NewItem`}>
-            <button className='w-full h-20 text-2xl indigo-outline'>
-                New Exercise
-            </button>
-            </Link>
-        </div>
+            <h1 className="text-4xl text-center mb-5">Exercises</h1>
+            {/*New Card Button */}
+            <div className='w-full flex justify-center'>
+                <Link 
+                    className='w-3/4'
+                    to={`editExercise/NewItem`}>
+                    <button className='w-full flex justify-around items-center h-20 text-2xl indigo-outline'>
+                        <FaRunning/> New Exercise <FaPlus/>
+                    </button>
+                </Link>
+            </div>
             {sortedDocs.map(doc => (
-                <ExerciseSwipable key={doc.name} doc={doc} handleDelete={handleDelete}/>
+                <ExerciseSwipable key={doc.name} doc={doc} handleDelete={handleDelete} />
             ))}
-        {/*New Card Button */}
-        <div className='w-full flex justify-center'>
-            <Link to ={`editExercise/NewItem`}>
-            <button className='w-full h-20 text-2xl indigo-outline'>
-                New Exercise
-            </button>
-            </Link>
         </div>
-    </div>
     );
 };
 export default PrintMyExercises;
--- fitness-app/src/components/containers/PrintMyRecipes.js
+++ fitness-app/src/components/containers/PrintMyRecipes.js
@@ -1,28 +1,29 @@
 import React, { useState } from 'react';
-import {Link, useNavigate} from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import useGetDocs from '../../hooks/useGetDocs';
 import './loading.css';
 import './containers.css';
 import './mobileCompatConfig.css';
 import { deleteDocument } from '../../firebase/firebaseFirestore';
 import RecipeSwipable from './RecipeSwipeable';
+import { FaPepperHot, FaPlus } from 'react-icons/fa';
 
-const PrintMyRecipes = ({path}) => {
-    const [isLoading, setLoading]= useState(false);
-    const {docs, loading, error} = useGetDocs(`${path}/recipes`);
+const PrintMyRecipes = ({ path }) => {
+    const [isLoading, setLoading] = useState(false);
+    const { docs, loading, error } = useGetDocs(`${path}/recipes`);
     //console.log(path)
 
-    const handleRecipeClick = (doc) =>{
+    const handleRecipeClick = (doc) => {
         //BreakPoint!! Learn how to manage states
         //Todo: handle logic to open a recipe item.  
         //Suggestion: make a recipe modal and call it here
         console.log('doc: ', doc);
     };
 
-    if(loading) return <p>Loading...</p>;
-    if(error) return <p>Error: {error}</p>;
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error: {error}</p>;
     const handleDelete = async (name) => {
-        if(window.confirm(`Are you sure you want to delete ${name}?`)){
+        if (window.confirm(`Are you sure you want to delete ${name}?`)) {
             setLoading(true);
             await deleteDocument('recipes', name);
             setLoading(false);
@@ -30,43 +31,36 @@ const PrintMyRecipes = ({path}) => {
         }
     }
 
-                    //{/*onClick={() => handleRecipeClick(doc)}*/} 
+    //{/*onClick={() => handleRecipeClick(doc)}*/} 
     const sortedDocs = docs.slice().sort((a, b) =>
         a.name.toLowerCase().localeCompare(b.name.toLowerCase())
     );
 
     return (
-    <div>
+        <div>
             {isLoading && (
                 <div className="loading-screen">
                     <div className="loading-spinner"></div>
                 </div>
             )}
-        <h1 className="text-4xl text-center mb-5">Recipes</h1>
-        {/*New Card Button */}
-        <div className='w-full flex justify-center'>
-            <Link to ={`editRecipe/NewItem`} state={`${{id: 'New Item', name: 'New Item', ingredients: 'Some ingredients', instructions: 'Some instructions'}}`}>
-            <button 
-            className='w-full h-20 text-2xl orange-outline'
-            >
-                New Recipe
-            </button>
-            </Link>
-        </div>
+            <h1 className="text-4xl text-center mb-5">Recipes</h1>
+            {/*New Card Button */}
+            <div className='w-full flex justify-center'>
+                <Link
+                    className='w-3/4'
+                    to={`editRecipe/NewItem`}
+                    state={`${{ id: 'New Item', name: 'New Item', ingredients: 'Some ingredients', instructions: 'Some instructions' }}`}>
+                    <button
+                        className='w-full flex justify-around items-center h-20 text-2xl orange-outline'
+                    >
+                        <FaPepperHot/> New Recipe <FaPlus/>
+                    </button>
+                </Link>
+            </div>
             {sortedDocs.map(doc => (
                 <RecipeSwipable key={doc.name} doc={doc} handleDelete={handleDelete}></RecipeSwipable>
             ))}
-        {/*New Card Button */}
-        <div className='w-full flex justify-center'>
-            <Link to ={`editRecipe/NewItem`} state={`${{id: 'New Item', name: 'New Item', ingredients: 'Some ingredients', instructions: 'Some instructions'}}`}>
-            <button 
-            className='w-full h-20 text-2xl orange-outline'
-            >
-                New Recipe
-            </button>
-            </Link>
         </div>
-    </div>
     );
 };
 export default PrintMyRecipes;
